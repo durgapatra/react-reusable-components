434b2a4a3b27c0de4dde55f3fef94594db792baf
durgap <durgap@lecturenotes.in>
2020-07-14 22:47:16 +0530

    react-components

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import  {StarRating}  from 'react-component'
+import 'react-component/dist/index.css'
+
+const App = () => {
+  return <StarRating/>
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { StarRating } from './Components'
+
+export { StarRating }
