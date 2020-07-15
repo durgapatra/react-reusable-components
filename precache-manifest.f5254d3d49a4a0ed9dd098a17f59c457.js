9e4f583d8fb6f02b146df8f389f9c9f77aa88750
durgap <durgap@lecturenotes.in>
2020-07-15 09:44:58 +0530

    Updates

---
+++ precache-manifest.f5254d3d49a4a0ed9dd098a17f59c457.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d059262f3bfaba872e8e9635eeeae0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "206f8f06901ff48de389",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "1df70ed13dce461aa589",
+    "url": "./static/js/2.b7d5e6eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b7d5e6eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "206f8f06901ff48de389",
+    "url": "./static/js/main.0872a569.chunk.js"
+  },
+  {
+    "revision": "5e40cddb96854904f905",
+    "url": "./static/js/runtime-main.1e56a23a.js"
+  }
+]);
